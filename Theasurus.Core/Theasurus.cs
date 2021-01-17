97891238d719701fa5f85a5b2ab4dda73f8ac672
Anna Rusanova <rusanova.anna.alexandrovna@gmail.com>
2021-01-17 18:04:02 +0100

    First version of the library

---
+++ Theasurus.Core/Theasurus.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Theasurus.Core
+{
+	public class Theasurus: ITheasurus
+	{
+		private TheasurusDbContext _dbContext;
+
+		public Theasurus(TheasurusDbContext dbContext)
+		{
+			_dbContext = dbContext;
+		}
+
+		public async Task AddSynonymsAsync(string word, IEnumerable<string> synonyms)
+		{
+			var wordId = (await GetWordOrCreate(word)).Id;
+			foreach(var synonymText in synonyms)
+			{
+				var synonymId = (await GetWordOrCreate(synonymText)).Id;
+				await _dbContext.SynonymMapping.AddAsync(new WordSynonym(wordId, synonymId));
+			}
+			await _dbContext.SaveChangesAsync();
+		}
+
+		public async Task<IEnumerable<string>> GetSynonymsAsync(string word)
+		{
+			var existingWord = GetWordOrNull(word);
+			if (existingWord == null)
+			{
+				throw new ArgumentException($"Word {word} is not present in the dictionary");
+			}
+
+			var synonymIds = await _dbContext.SynonymMapping.Where(x => x.WordId == existingWord.Id).Select(x => x.SynonymId).ToListAsync();
+			return await _dbContext.Words.Where(x => synonymIds.Contains(x.Id)).Select(x => x.Text).ToListAsync();
+		}
+
+		public async Task<SearchResult> GetWordsAsync(uint take, uint skip)
+		{
+			var total = _dbContext.Words.Count();
+			var result = await _dbContext.Words.Skip((int)skip).Take((int)take).ToListAsync();
+			var next = skip + take;
+
+			return new SearchResult(result.Select(x => x.Text), total, next < total ? next : null);
+		}
+
+		private Task<Word> GetWordOrNull(string text) => 
+			_dbContext.Words.FirstOrDefaultAsync(x => string.Equals(text, x.Text, StringComparison.OrdinalIgnoreCase));
+
+		private async Task<Word> GetWordOrCreate(string text)
+		{
+			var word = await GetWordOrNull(text);
+			if(word == null)
+			{
+				return (await _dbContext.Words.AddAsync(new Word(text))).Entity;
+			}
+
+			return word;
+		}
+	}
+}
---
+++ Theasurus.Core/TheasurusDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Theasurus.Core
+{
+	public class TheasurusDbContext: DbContext
+	{
+		public DbSet<Word> Words { get; set; } = default!;
+		public DbSet<WordSynonym> SynonymMapping { get; set; } = default!;
+	}
+
+	public record Word
+	{
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int Id { get; set; }
+
+		public string Text { get; init; }
+
+		public Word(string text)
+		{
+			Text = text;
+		}
+	};
+
+	public record WordSynonym(int WordId, int SynonymId);
+}
